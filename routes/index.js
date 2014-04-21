53edb5a48998a4487caa2303b76fad35a6944032
serewit <root@serewit.com>
2014-04-21 15:18:57 -0400

    activation skeleton working for the most part

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var xmlparser = require('express-xml-bodyparser');
+var builder = require('xmlbuilder');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -14,8 +15,30 @@ router.get('/helloworld', function(req, res) {
 
 /* POST activation */
 router.post('/activation', xmlparser({trim: false, explicitArray: false}), function(req, res, next){
-	console.log(req.body);
-	res.send(200);
-})
+	var data = req.body;
+	var db = req.db;
+	var activation_response = '<ted500ActivationResponse>\n';
+	activation_response += '<PostServer>wattchr.austinlparker.com</PostServer>\n';
+	activation_response += '<UseSSL>F</UseSSL>\n';
+	activation_response += '<PostPort>3000</PostPort>\n';
+	activation_response += '<PostURL>/postdata</PostURL>\n';
+	activation_response += '<AuthToken></AuthToken>\n';
+	activation_response += '<PostRate>5</PostRate>\n';
+	activation_response += '<HighPrec>F</HighPrec>\n';
+	activation_response += '</ted500ActivationResponse>\n';
+
+	console.log(data['ted5000Activation'].Gateway);
+	console.log(activation_response);
+	var new_gateway = { gateway: data['ted5000Activation'].Gateway }
+	db.collection('gateways').insert(new_gateway, function(err) {
+		if(err) {
+			return console.log('insert error', err);
+		}
+	});
+	res.writeHead(200, {'Content-Type': 'application/xml'} );
+	res.end(activation_response);
+});
 
 module.exports = router;
+
+
